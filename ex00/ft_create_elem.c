1c1779c9a003ace7e208a62233a47f0811c7e222
Fong Yan Xiang <yfon0008@student.monash.edu>
2024-01-31 01:20:28 +0800

    corrected ex00 & 01

--- ex00/ft_create_elem.c
+++ ex00/ft_create_elem.c
@@ -1,8 +1,14 @@
 #include "ft_list.h"
+#include <stdlib.h>
 
 t_list	*ft_create_elem(void *data)
 {
     t_list	*temp;
+
+	temp = (t_list *) malloc (sizeof(t_list));
+
+	if (temp == 0)
+		return 0;	
 
 	temp->data = data;
 	temp->next = 0;
--- ex01/ft_list_push_front.c
+++ ex01/ft_list_push_front.c
@@ -5,6 +5,8 @@ void	ft_list_push_front(t_list **begin_list, void *data)
 	t_list	*temp;
 
 	temp = ft_create_elem(data);
+	if (temp == 0)
+		return ;
 	temp->next = begin_list[0];
 	begin_list[0] = temp;
 }
